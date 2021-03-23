376b493e8b478b55de028abc679c6c4075077eba
Sami Hadouaj <samihadouaj@Samis-MacBook-Pro.local>
2021-03-23 13:25:26 +0100

    added method to person

--- src/main/java/org/iselab/grocery/domain/Customer.java
+++ src/main/java/org/iselab/grocery/domain/Customer.java
@@ -5,4 +5,16 @@ public class Customer extends Person{
             System.out.println("THIS MY NAME"+ name);
             return name;
         }
+
+    @Override
+    double calculateSalary() {
+        System.out.println("We are calculating your salary... if it exits :p: ");
+        double salary = 74*0.54+43435;
+        return  salary;
+    }
+
+    @Override
+    String areYouHuman() {
+        return "yes of course sir! you are our cutormer";
+    }
 }
