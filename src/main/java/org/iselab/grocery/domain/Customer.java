729543b47cff3a4e0f7c89cab0e8431641c46ed1
Sami Hadouaj <samihadouaj@Samis-MacBook-Pro.local>
2021-03-23 13:20:49 +0100

    added method to custormer class

--- src/main/java/org/iselab/grocery/domain/Customer.java
+++ src/main/java/org/iselab/grocery/domain/Customer.java
@@ -1,5 +1,8 @@
 package org.iselab.grocery.domain;
 
 public class Customer extends Person{
-
+        public String sayMyName(String name){
+            System.out.println("THIS MY NAME"+ name);
+            return name;
+        }
 }
